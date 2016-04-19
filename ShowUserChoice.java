395dc45676a3f6cba07492c7d1cb4e831be967ed
demidiuc <serg.demidiuc@gmail.com>
2016-04-19 14:11:40 +0300

    Create ShowUserChoice.java
    Add ShowUserChoice.java

---
+++ ShowUserChoice.java
@@ -0,0 +1,37 @@
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class ShowUserChoice {
+
+    public static int getDataFromConsole() {
+        Scanner cs = new Scanner(System.in);
+
+        System.out.println("Enter the text:\n");
+        String text = cs.nextLine();
+
+        try{
+            return Integer.parseInt(text);
+        }
+        catch (NumberFormatException e){
+            System.out.println("Err: Введенный символ не является целым числом\n");
+        }
+        return getDataFromConsole();
+    }
+
+    public static void setupDefaultValues() {
+        HashMap<Integer, String> figureNumber = new HashMap<Integer, String>();
+        figureNumber.put(1, "Круг");
+        figureNumber.put(2, "Треугольник");
+        figureNumber.put(3, "Квадрат");
+        figureNumber.put(4, "Прямоугольник");
+        figureNumber.put(5, "Ромб");
+        figureNumber.put(6, "Трапеция");
+
+        String figure = figureNumber.get(getDataFromConsole());
+        System.out.println(figure);
+    }
+
+    public static void main(String[] args) {
+        setupDefaultValues();
+    }
+}
