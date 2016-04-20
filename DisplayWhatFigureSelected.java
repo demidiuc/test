dacdcb84a731f5e9b89c49010a2defcbbde85ca8
demidiuc <serg.demidiuc@gmail.com>
2016-04-20 15:38:49 +0300

    Update DisplayWhatFigureSelected.java
    +

--- DisplayWhatFigureSelected.java
+++ DisplayWhatFigureSelected.java
@@ -1,5 +1,3 @@
-package Scanner;
-
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
@@ -7,8 +5,7 @@ import java.util.Scanner;
 public class DisplayWhatFigureSelected {
 
     public String getFigureName() {
-        //Integer xtext = getDataFromConsole();
-        Integer xtext = 1;
+        Integer xtext = getDataFromConsole();
         Map<Integer, String> xFigureg = getMapWithFigures();
 
         return xFigureg.get(xtext);
@@ -17,14 +14,14 @@ public class DisplayWhatFigureSelected {
     public Integer getDataFromConsole() {
         Scanner cs = new Scanner(System.in);
 
-        System.out.println("Enter the text:\n");
+        System.out.println("Enter the number from 1 to 6:");
         String text = cs.nextLine();
 
         try{
             return Integer.parseInt(text);
         }
         catch (NumberFormatException e){
-            System.out.println("Err: Введенный символ не является целым числом\n");
+            System.out.println("Err: character is not an integer\n");
         }
 
         return getDataFromConsole();
@@ -32,12 +29,12 @@ public class DisplayWhatFigureSelected {
 
     public Map<Integer, String> getMapWithFigures() {
         HashMap<Integer, String> figureNumber = new HashMap<Integer, String>();
-        figureNumber.put(1, "Круг");
-        figureNumber.put(2, "Треугольник");
-        figureNumber.put(3, "Квадрат");
-        figureNumber.put(4, "Прямоугольник");
-        figureNumber.put(5, "Ромб");
-        figureNumber.put(6, "Трапеция");
+        figureNumber.put(1, "Circle");
+        figureNumber.put(2, "Triangle");
+        figureNumber.put(3, "Square");
+        figureNumber.put(4, "Rectangle");
+        figureNumber.put(5, "Rhombus");
+        figureNumber.put(6, "Trapeze");
         return figureNumber;
     }
 
@@ -45,6 +42,6 @@ public class DisplayWhatFigureSelected {
 
     public static void main(String[] args) {
         DisplayWhatFigureSelected xu = new DisplayWhatFigureSelected();
-        System.out.println(xu.getFigureName());
+        System.out.println("Your choise is: " + xu.getFigureName());
     }
 }
